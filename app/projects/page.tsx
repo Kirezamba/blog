14b8fb1fe4ebf15ae75a4d34319d68d935b67ea7
Kirill Elaev <k.elaev@intelcomline.ru>
2023-09-06 15:48:47 +0300

    read me change

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -11,9 +11,7 @@ export default async function ProjectsPage() {
       <div className='mt-5 px-6 pt-16 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32'>
         <div className='max-w-2xl mx-auto lg:mx-0'>
           <h2 className='text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl'>Товары</h2>
-          <p className='mt-4 text-zinc-400'>
-            Some of the projects are from work and some are on my own time.
-          </p>
+          <p className='mt-4 text-zinc-400'>rarrikate | немецкий язык | deutsch</p>
         </div>
         <div className='w-full h-px bg-zinc-800' />
 
@@ -21,8 +19,6 @@ export default async function ProjectsPage() {
           {projects.map(card => (
             <ProjectCard key={card.slug} {...card} />
           ))}
-
-          {/* <div className='flex flex-col w-full gap-8 mx-auto border-t border-gray-900/10 lg:mx-0 lg:border-t-0 '></div> */}
         </div>
         <div className='hidden w-full h-px md:block bg-zinc-800' />
       </div>
