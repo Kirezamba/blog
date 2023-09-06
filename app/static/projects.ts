edcce60a340b198ce1fe7834ff22d356d17f18d7
Kirill Elaev <k.elaev@intelcomline.ru>
2023-09-06 12:42:14 +0300

    content add

---
+++ app/static/projects.ts
@@ -0,0 +1,77 @@
+import { GoodsCardProps } from "../projects/projectCard";
+
+export const projects: GoodsCardProps[] = [
+  {
+    slug: "club",
+    title: "Закрытый клуб",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo ratione delectus eos aut amet officia facere commodi aliquam at sed et nam laudantium nesciunt,excepturi inventore ea deleniti corporis? Pariatur!",
+    price: 5000,
+    benefits: [
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt itaque ipsa quae aspernatur ut delectus quidem dolore, ex impedit, odit earum veritatis amet eius ducimus numquam quo facere illum. Necessitatibus.",
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt itaque ipsa quae aspernatur ut delectus quidem dolore, ex impedit, odit earum veritatis amet eius ducimus numquam quo facere illum. Necessitatibus.",
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt itaque ipsa quae aspernatur ut delectus quidem dolore, ex impedit, odit earum veritatis amet eius ducimus numquam quo facere illum. Necessitatibus.",
+    ],
+  },
+
+  {
+    slug: "mentoring",
+    title: "Наставничество",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo ratione delectus eos aut amet officia facere commodi aliquam at sed et nam laudantium nesciunt,excepturi inventore ea deleniti corporis? Pariatur!",
+    price: 5000,
+    benefits: [
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt itaque ipsa quae aspernatur ut delectus quidem dolore, ex impedit, odit earum veritatis amet eius ducimus numquam quo facere illum. Necessitatibus.",
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt itaque ipsa quae aspernatur ut delectus quidem dolore, ex impedit, odit earum veritatis amet eius ducimus numquam quo facere illum. Necessitatibus.",
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt itaque ipsa quae aspernatur ut delectus quidem dolore, ex impedit, odit earum veritatis amet eius ducimus numquam quo facere illum. Necessitatibus.",
+    ],
+  },
+  {
+    slug: "group-mentoring",
+    title: "Групповое наставничество",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo ratione delectus eos aut amet officia facere commodi aliquam at sed et nam laudantium nesciunt,excepturi inventore ea deleniti corporis? Pariatur!",
+    price: 5000,
+    benefits: [
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt itaque ipsa quae aspernatur ut delectus quidem dolore, ex impedit, odit earum veritatis amet eius ducimus numquam quo facere illum. Necessitatibus.",
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt itaque ipsa quae aspernatur ut delectus quidem dolore, ex impedit, odit earum veritatis amet eius ducimus numquam quo facere illum. Necessitatibus.",
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt itaque ipsa quae aspernatur ut delectus quidem dolore, ex impedit, odit earum veritatis amet eius ducimus numquam quo facere illum. Necessitatibus.",
+    ],
+  },
+  {
+    slug: "be-part-of-team",
+    title: "Стать частью моей команды",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo ratione delectus eos aut amet officia facere commodi aliquam at sed et nam laudantium nesciunt,excepturi inventore ea deleniti corporis? Pariatur!",
+    price: 5000,
+    benefits: [
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt itaque ipsa quae aspernatur ut delectus quidem dolore, ex impedit, odit earum veritatis amet eius ducimus numquam quo facere illum. Necessitatibus.",
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt itaque ipsa quae aspernatur ut delectus quidem dolore, ex impedit, odit earum veritatis amet eius ducimus numquam quo facere illum. Necessitatibus.",
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt itaque ipsa quae aspernatur ut delectus quidem dolore, ex impedit, odit earum veritatis amet eius ducimus numquam quo facere illum. Necessitatibus.",
+    ],
+  },
+  {
+    slug: "deutsch-group",
+    title: "Групповые занятия (Немецкий)",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo ratione delectus eos aut amet officia facere commodi aliquam at sed et nam laudantium nesciunt,excepturi inventore ea deleniti corporis? Pariatur!",
+    price: 5000,
+    benefits: [
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt itaque ipsa quae aspernatur ut delectus quidem dolore, ex impedit, odit earum veritatis amet eius ducimus numquam quo facere illum. Necessitatibus.",
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt itaque ipsa quae aspernatur ut delectus quidem dolore, ex impedit, odit earum veritatis amet eius ducimus numquam quo facere illum. Necessitatibus.",
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt itaque ipsa quae aspernatur ut delectus quidem dolore, ex impedit, odit earum veritatis amet eius ducimus numquam quo facere illum. Necessitatibus.",
+    ],
+  },
+  {
+    slug: "deutsch-individual",
+    title: "Индивидуальные занятия (Немецкий)",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo ratione delectus eos aut amet officia facere commodi aliquam at sed et nam laudantium nesciunt,excepturi inventore ea deleniti corporis? Pariatur!",
+    price: 5000,
+    benefits: [
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt itaque ipsa quae aspernatur ut delectus quidem dolore, ex impedit, odit earum veritatis amet eius ducimus numquam quo facere illum. Necessitatibus.",
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt itaque ipsa quae aspernatur ut delectus quidem dolore, ex impedit, odit earum veritatis amet eius ducimus numquam quo facere illum. Necessitatibus.",
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt itaque ipsa quae aspernatur ut delectus quidem dolore, ex impedit, odit earum veritatis amet eius ducimus numquam quo facere illum. Necessitatibus.",
+    ],
+  },
+];
