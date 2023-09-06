595f45e53c955b58a545e9832ad686095a00d215
Kirill Elaev <k.elaev@intelcomline.ru>
2023-09-06 15:48:47 +0300

    read me change

--- app/page.tsx
+++ app/page.tsx
@@ -32,26 +32,12 @@ export default function Home() {
       </h1>
 
       <div className='hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0' />
-      <div className='my-16 text-center animate-fade-in'>
-        <h2 className='text-sm text-zinc-500 '>
-          Hi, my name is Andreas, I'm building serverless and open source solutions at{" "}
-          <Link
-            target='_blank'
-            href='https://upstash.com'
-            className='underline duration-500 hover:text-zinc-300'
-          >
-            Upstash
-          </Link>
+      <div className='my-16 text-center animate-fade-in overflow-hidden relative'>
+        <h2 className='text-sm text-zinc-500 mt-4 mb-10 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300'>
+          Привет! Я Катя и уже 5 лет я преподаю немецкий язык!
           <br />
-          and working on{" "}
-          <Link
-            target='_blank'
-            href='https://unkey.dev'
-            className='underline duration-500 hover:text-zinc-300'
-          >
-            unkey.dev
-          </Link>{" "}
-          at night.
+          И все эти пять лет мои ученики успешно преодолевают языковой барьер, <br /> сдают
+          экзамены, переезжают и устраиваются на перспективные должности 🔥
         </h2>
       </div>
     </div>
